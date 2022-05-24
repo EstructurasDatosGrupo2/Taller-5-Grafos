52c48ba1f977fa13569b69202a819c12950e606e
Alejandro Sacristan Leal <alejandro-sacristan@javeriana.edu.co>
2022-05-23 22:36:41 -0500

    eureka

---
+++ main.cpp
@@ -0,0 +1,112 @@
+#include "Graph.h"
+#include <cstdlib>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+queue<int> uploadStellarMap(string fileName);
+void saveConstellationsTack(string fileName);
+
+int main(){
+
+    Graph<int, int> myGraph(2);
+    queue<int> queueStellarMap;
+    char matrixAdj[29][29];
+    /*
+    cout<<"Hello the Stellar Traker program"<<endl;
+    myGraph.agregarArista(2, 3);
+    myGraph.agregarArista(2, 3);
+    myGraph.agregarArista(2, 4, 0, 1);
+    myGraph.agregarArista(2, 4, 3);
+    cout << myGraph.printGraph();
+
+    Graph<int> grafo(1);
+    grafo.agregarArista(1, 2);
+    grafo.agregarArista(1, 6);
+    grafo.agregarArista(2, 3);
+    grafo.agregarArista(2, 4);
+    grafo.agregarArista(3, 4);
+    grafo.agregarArista(5, 4);
+    grafo.agregarArista(6, 5);
+    cout << grafo.printGraph();
+    cout<<grafo.cantidadCamposConectados()<<endl;
+   */
+
+    queueStellarMap = uploadStellarMap("stellarMap.txt");
+    cout << endl;
+    cout << endl;
+   
+    for(int filas = 0; filas < 28; filas++){
+
+        for (int columnas = 0; columnas < 28; columnas++){
+            matrixAdj[filas][columnas] = queueStellarMap.front();
+            cout << queueStellarMap.front() << " ";
+            queueStellarMap.pop();
+            
+        }
+        cout << endl;
+    }
+
+    
+    
+
+
+  
+   
+}
+
+queue<int> uploadStellarMap(string fileName){
+    
+    queue<int> queueStellarMap;
+    char *ch = new char[1040];
+    char * pch;
+	ifstream file;
+    
+	//open file
+	file.open(fileName,ios::in);
+	if(!file)
+	{
+		cout<<"Error in opening file!!!"<<endl;
+		
+	}
+	
+	//read and print file content
+	while (!file.eof()) 
+	{
+		file.getline(ch,1040,',');	//reading from file
+		cout << ch << " ";	//printing
+        int cost = atoi(ch);
+        queueStellarMap.push(cost);
+     
+        
+	}
+	//close the file
+	file.close();
+   
+
+    return queueStellarMap;
+}
+
+void saveConstellationsTack(string fileName){
+    fstream newfile;
+    newfile.open(fileName,ios::out);  // open a file to perform write operation using file object
+    if(newfile.is_open())     //checking whether the file is open
+    {
+        newfile<<"Tutorials point \n"; //inserting text
+        newfile.close(); //close the file object
+    }
+    newfile.open(fileName,ios::in); //open a file to perform read operation using file object
+    if (newfile.is_open()){   //checking whether the file is open
+        string tp;
+        while(getline(newfile, tp)){  //read data from file object and put it into string.
+            cout << tp << "\n";   //print the data of the string
+        }
+        newfile.close();   //close the file object.
+    }
+}
+
